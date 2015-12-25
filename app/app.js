41033d853c955bf2419c26950b8ccd6cf0dd92ea
WanderWang <wander.wang@egret.com>
2015-12-25 14:29:55 +0800

    tiny

--- app/app.js
+++ app/app.js
@@ -3,36 +3,14 @@ require("mybootstrap");
 var webpack = require("webpack");
 var fs = require("fs");
 var CodeMirror = require("code-mirror");
+var path = require("path");
 window.fsData = fs.data;
+window.fs = fs;
 
-$(function() {
-	
-	var currentFileInEditor = "entry.js";
-	var editor = CodeMirror($(".editor")[0], {
-		value: fs.readFileSync(fs.join("/folder", currentFileInEditor)).toString(),
-		mode: "javascript"
-	});
-
-	$(".create-new-file").click(function() {
-		var filename = prompt("Filename?", "file.js");
-		if(!filename) return;
-		fs.writeFile(fs.join("/folder", filename), new Buffer("New File", "utf-8"), function(err) {
-			if(err) return alert(err.stack);
-			loadFile(filename);
-			rebuildTabs();
-		});
-	});
+$(function () {
 	
 	$(".compile").click(compile);
 
-	$(".files").delegate(".file", "click", function() {
-		var filename = $(this).data("file");
-		$(".files .file.active").removeClass("active");
-		$(this).addClass("active");
-		loadFile(filename);
-		return false;
-	});
-	
 	var compiler = webpack({
 		inputFileSystem: fs,
 		outputFileSystem: fs,
@@ -50,46 +28,19 @@ $(function() {
 			filename: "bundle.js"
 		}
 	});
-	
-	rebuildTabs();
+
 	compile();
 
-	function rebuildTabs() {
-		var files = fs.readdirSync("/folder");
-		var filesElement = $(".files");
-		filesElement.find(".file").remove();
-		files.forEach(function(filename) {
-			var fileElement = $("<li class='file'>").data("file", filename);
-			if(filename === currentFileInEditor) fileElement.addClass("active");
-			fileElement.append($("<a href='#'>").text(filename));
-			filesElement.prepend(fileElement);
-		});
-	}
-	
 	function compile() {
-		saveFile(function() {
-			compiler.run(function(err, stats) {
-				if(err) return $(".stats").text(err.stack);
-				$(".stats").text(stats.toString());
-				$("iframe")[0].srcdoc = "<html><body><script>" + fs.readFileSync("/output/bundle.js").toString() + "</script></body></html>";
-			});
+		compiler.run(function (err, stats) {
+			if (err) return $(".stats").text(err.stack);
+			$(".stats").text(stats.toString());
+			updatePreview();
 		});
 	}
 
-	function loadFile(filename) {
-		saveFile(function() {
-			var buffer = fs.readFileSync(fs.join("/folder", filename));
-			var str = typeof buffer === "string" ? buffer : buffer.toString();
-			currentFileInEditor = filename;
-			editor.setValue(str);
-		});
-	}
-
-	function saveFile(callback) {
-		var value = editor.getValue();
-		fs.writeFile(fs.join("/folder", currentFileInEditor), new Buffer(value, "utf-8"), function(err) {
-			if(err) return alert(err);
-			callback();
-		});
+	function updatePreview() {
+		var bundle = fs.readFileSync("/output/bundle.js").toString()
+		$("iframe")[0].srcdoc = "<html><body><script>" + bundle + "</script></body></html>";
 	}
 });
--- web_modules/fs.js
+++ web_modules/fs.js
@@ -40,7 +40,7 @@ var filesystem = exports.data = {
 	},
 	"folder": {
 		"": true,
-		"entry.js": new Buffer("var a = require('./a.tsx');\nconsole.log(a)", "utf-8"),
+		"entry.js": new Buffer("//var a = require('./a.tsx');\nconsole.log(a)", "utf-8"),
 		"a.tsx": new Buffer("var A = (<div>a</div>);export = A", "utf-8"),
 		"style.css": new Buffer("body {\n\tbackground: #333;\n\tcolor: #EEE;\n}", "utf-8")
 	}
